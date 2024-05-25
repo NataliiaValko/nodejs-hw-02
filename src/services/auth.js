e9a52735cbd1f3993c6b5a8149ac3e2edf91bba2
NataliiaValko <tanarisoffice@gmail.com>
2024-05-25 17:44:47 +0300

    add 409 status

--- src/services/auth.js
+++ src/services/auth.js
@@ -7,6 +7,9 @@ import { Session } from '../db/models/session.js';
 import { User } from '../db/models/user.js';
 
 export const registerUser = async (payload) => {
+  const user = await User.findOne({ email: payload.email });
+  if (user) throw createHttpError(409, 'Email in use');
+
   const encryptedPassword = await bcrypt.hash(payload.password, 10);
 
   return await User.create({
